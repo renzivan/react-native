cf1135c7bc4799d41aa0adb43414bb6e31d3b1f8
renzivan <renzivanyapenguio@gmail.com>
2024-01-29 02:04:28 +0800

    minor updates

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import FormLogin from './pages/login';
+import LoginPage from './pages/login';
 import Brand from './components/Brand';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Brand />
-      <FormLogin />
+      <LoginPage />
       <StatusBar style="auto" />
     </View>
   );
