a2e4997b16b3abad5282c7de063251a3b5579ce1
renzivan <renzivanyapenguio@gmail.com>
2024-01-28 22:15:49 +0800

    added login page

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FormLogin from './pages/login';
 import Brand from './components/Brand';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Brand />
+      <FormLogin />
       <StatusBar style="auto" />
     </View>
   );
---
+++ pages/login.tsx
@@ -0,0 +1,69 @@
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClickLogin = () => {
+    console.log('email: ', email);
+    console.log('password: ', password);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Email: </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(val) => setEmail(val)}
+        />
+      </View>
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Password: </Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Password"
+          onChangeText={(val) => setPassword(val)}
+        />
+      </View>
+      <Pressable style={styles.button} onPress={handleClickLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'stretch',
+    gap: 20,
+    paddingHorizontal: 40,
+  },
+  inputGroup: {
+    gap: 5
+  },
+  label: {
+    color: '#333',
+  },
+  input: {
+    borderColor: 'gray',
+    borderRadius: 10,
+    borderWidth: 1,
+    height: 40,
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: '#2196f3',
+    borderRadius: 100,
+    height: 40,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+  }
+});
