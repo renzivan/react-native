80a3571970ff2e78cf437160b9bdbb029b7ba3e0
renzivan <renzivanyapenguio@gmail.com>
2024-01-29 03:09:52 +0800

    setup expo router

--- App.tsx
+++ App.tsx
@@ -1,23 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginPage from './pages/login';
-import Brand from './components/Brand';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import LoginPage from './pages/login';
+// import Brand from './components/Brand';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Brand />
-      <LoginPage />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Brand />
+//       <LoginPage />
+//       <Text onPress={() => {console.log('signup')}}>Signup here!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     alignItems: 'center',
+//     backgroundColor: '#fff',
+//     flex: 1,
+//     justifyContent: 'center',
+//   }
+// });
